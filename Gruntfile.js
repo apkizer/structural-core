1aef2abea83a832cb0a3407de703bd5aa751bb3b
unknown <de1y@WPWDRK1G.OPR.STATEFARM.ORG>
2014-07-09 11:44:32 -0500

    Setup grunt and added Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            options: {
+                banner: '/* Structural, by Alex Kizer */ \n'
+            },
+            dist: {
+                src: ['js/*.js'],
+                dest: 'structural.js'
+            }
+        },
+        uglify: {
+            options: {
+            }, 
+            dist: {
+                files: {
+                    'structural.min.js': ['structural.js']
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
