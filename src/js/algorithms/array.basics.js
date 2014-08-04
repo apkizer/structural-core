e5e418bfa37150253616905b0fcf4920983dd6c7
Alex Kizer <the.alex.kizer@gmail.com>
2014-08-03 21:42:36 -0500

    worked on array algorithms

--- src/js/algorithms/array.basics.js
+++ src/js/algorithms/array.basics.js
@@ -1,7 +1,14 @@
-S.method('array', 'finish', function(){
+S.method('array', 'finish', function() {
   this.clearfocus();
   this.leftTo(0);
-})
+});
+
+S.method('array', 'swap', function(a, b) {
+  this.log('swapping');
+  this('temp', this.getItem(a));
+  this.setItem(a, this.getItem(b));
+  this.setItem(b, this('temp'));
+});
 
 S.method('array', 'searchLinear', function(target) {
   for(var i = 0; i < this.getLength(); i++) {
@@ -17,3 +24,4 @@ S.method('array', 'searchLinear', function(target) {
 S.method('array', 'searchBinary', function(target) {
   // TODO
 });
+
--- src/js/algorithms/array.sorts.js
+++ src/js/algorithms/array.sorts.js
@@ -3,10 +3,11 @@ S.method('array', 'insertionSort', function(){
 	    this('j', i);  
 	    while(this('j') > 0 && this.getItem(this('j') - 1) > this.getItem(this('j'))) {
 	        //swap:
-	        this.log('swapping');
+	        /*this.log('swapping');
 	        this('temp', this.getItem(this('j')));
 	        this.setItem(this('j'), this.getItem(this('j') - 1));
-	        this.setItem(this('j') - 1, this('temp'));
+	        this.setItem(this('j') - 1, this('temp'));*/
+          this.swap(this('j'), this('j') - 1);
 	        this('swapped', true);
 	        this('j', this('j') - 1);
 	    }
@@ -24,12 +25,13 @@ S.method('array', 'bubbleSort', function(){
       this.focus(i);
       if( this.getItem(i) > this.getItem(i + 1) ) {
         // swap
-        this.range(i, i+1, 1);
-        this('temp', this.getItem(i));
+        this.range(i, i+1, 2);
+        /*this('temp', this.getItem(i));
         this.setItem(i, this.getItem(i + 1));
-        this.setItem(i + 1, this('temp'));
+        this.setItem(i + 1, this('temp'));*/
+        this.swap(i, i+1);
         this('swap', true);
-        this.clearrange(1);
+        this.clearrange(2);
       }
     }
   }
