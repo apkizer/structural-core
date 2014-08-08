8b820d1f36963ec2471bc38337fa572a427ef2bb
unknown <the.alex.kizer@gmail.com>
2014-08-08 15:32:16 -0500

    refactored tree.js

--- src/js/components/tree.js
+++ src/js/components/tree.js
@@ -1,85 +1,74 @@
 S.component('tree', function (tree, view) {
   var c = S.base(),
-    nodes = S.map(),
-    height = 0;
-  c.tree = new Node(0);
+      height = 0;
 
-  c.live.clearfocus = null;
-
-  function Node(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.sid = S.nextId();
+  function node(value) {
+    return {
+      value: value,
+      left: null,
+      right: null,
+      sid: S.nextId()
+    };
   }
 
   c.init = function() {
-    console.log('init tree');
     c.tree = copyTree(tree);
-    height = determineHeight(c.tree);
-    computeHeight(c.tree);
-  }
-
-  function copyTree(node) {
-    if(!node)
-      return null;
-    var n = new Node(node.value);
-    n.left = copyTree(node.left);
-    n.right = copyTree(node.right);
-    return n;
-  }
-  
-  function computeHeight(root) {
-    console.log('computing height');
-    if(root) {
-      console.log('height is ' +  (1 + Math.max(computeHeight(root.left), computeHeight(root.right))));
-      root.height = 1 + Math.max(computeHeight(root.left), computeHeight(root.right));
-      return root.height;
-    } else {
-      return -1;
-    }
-  }
-  
-  function determineHeight(root) {
-    if(!root)
-      return -1;
-    return 1 + Math.max(determineHeight(root.left), determineHeight(root.right));
-    
+    height = computeHeights(c.tree);
+    computeHeights(c.tree);
   }
       
   c.height = function() {
     return height;
   }
+  
+  function copyTree(_node) {
+    if(!_node) return null;
+    var n = node(_node.value);
+    n.left = copyTree(_node.left);
+    n.right = copyTree(_node.right);
+    return n;
+  }
+  
+  function computeHeights(root) {
+    if(root)
+      return root.height = 1 + Math.max(computeHeights(root.left), computeHeights(root.right));
+    return -1;
+  }
 
   c.live.root = function() {
     if(c.tree)
-      return c.tree;//.sid;
+      return c.tree;
+  }
+  
+  c.live.height = function() {
+    return height;
   }
   
   c.live.add = function(parent, direction, value) {
     if(direction) {
-      parent.right = new Node(value);
+      parent.right = node(value);
     } else {
-      parent.left = new Node(value);
+      parent.left = node(value);
     }
-    computeHeight(c.tree);
-  }
+    computeHeights(c.tree);
+    // TODO if avl, avl stuff here  
+  } 
   
-  c.live.traverse = null;
+  c.live.clearfocus = null;
   
   c.live.travel = null;
   
   c.live.label = null;
-
+  
   c.live.focusOn = null;
   
   c.live.setNode = function(node, value) {
     node.value = value;
-  }
+  }; 
   
   c.live.isBinary = function() {
     return checkBST(c.tree);
-  }
+  };
   
   function checkBST(root) {
     if(root) {
@@ -92,8 +81,7 @@ S.component('tree', function (tree, view) {
       }
       return ret && checkBST(root.left) && checkBST(root.right);
     } else {
-      // null tree is vacuously BST
-      return true; 
+      return true; // null tree is vacuously BST
     }
   }
   
@@ -101,6 +89,7 @@ S.component('tree', function (tree, view) {
     return S.getComponentMethods('tree');
   }
   
+  // TODO, add copy method
   c.noCopy = true;
 
   return c;
