c42f1ed40e510f3c3b1aaefa3f2ae871acc88431
Alex Kizer <the.alex.kizer@gmail.com>
2014-09-07 13:58:06 -0500

    updated gruntfile; added extra newline separator

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         },
         concat: {
             options: {
-                separator: '\n'
+                separator: '\n\n'
             },
             js: {
                 src: [
